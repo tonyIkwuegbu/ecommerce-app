9b5661308be59000a61130ea864d51b53d55d67f
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-06-07 12:08:04 +0100

    chores

--- src/components/MainPage/SlideCard.jsx
+++ src/components/MainPage/SlideCard.jsx
@@ -1,4 +1,4 @@
-import Sdata from "./Slidedata";
+import Sdata from "./SlideData";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
