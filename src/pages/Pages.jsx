9069d5b11b26dcc5c4c65528efa03a72191d4cce
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-06-14 17:16:46 +0100

    update routes

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,14 +1,18 @@
 import Home from "../components/MainPage/Home";
-import Discount from "../components/discount/Discount";
+import Discount from "../components/newarrivals/Discount";
 import FlashDeals from "../components/flashDeals/FlashDeals";
 import NewsLetter from "../components/newsletter/NewsLetter";
+import MainTop from "../components/Top/MainTop";
+import TopDeals from "../components/deals/DealsMain";
 
 // import Wrapper from "../components/wrapper/Wrapper";
 
-const Pages = ({ productItems }) => {
+const Pages = ({ productItems, shopItems }) => {
 	return (
 		<>
 			<Home />
+			<MainTop />
+			<TopDeals shopItems={shopItems} />
 			<FlashDeals productItems={productItems} />
 			<Discount />
 			<NewsLetter />
