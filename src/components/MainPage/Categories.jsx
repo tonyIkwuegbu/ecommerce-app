a827ee9da7187ed53fb941c20117010e0cffdf42
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-07-15 18:59:16 +0100

    refac: update component

--- src/components/MainPage/Categories.jsx
+++ src/components/MainPage/Categories.jsx
@@ -1,4 +1,5 @@
 import Items from "../../NavData";
+import { Link } from "react-router-dom";
 
 const Categories = () => {
 	return (
@@ -7,12 +8,14 @@ const Categories = () => {
 				{Items.map((value) => {
 					return (
 						<div className="box" key={value.key}>
-							<p className="flex items-center leading-10">
-								<span>
-									<value.icon className="text-lg" />
-								</span>
-								{value.label}
-							</p>
+							<Link to={value.path}>
+								<p className="flex items-center leading-10">
+									<span>
+										<value.icon className="text-lg" />
+									</span>
+									{value.label}
+								</p>
+							</Link>
 						</div>
 					);
 				})}
