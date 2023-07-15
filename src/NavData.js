c6ce937cca351d50c4ce61c7229f8b0f9292e974
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-07-15 20:47:49 +0100

    fix: bug fix

--- src/NavData.js
+++ src/NavData.js
@@ -14,37 +14,37 @@ const Items = [
 		key: "2",
 		label: "Women",
 		icon: FcBusinesswoman,
-		path: "/category/women",
+		path: "/#",
 	},
 	{
 		key: "3",
 		label: "Children",
 		icon: FaBaby,
-		path: "/category/children",
+		path: "/#",
 	},
 	{
 		key: "4",
 		label: "Health & Beauty",
 		icon: GiHealthIncrease,
-		path: "/category/health",
+		path: "/#",
 	},
 	{
 		key: "5",
 		label: "Home & Lights",
 		icon: GiOilySpiral,
-		path: "/category/home-essentials",
+		path: "/#",
 	},
 	{
 		key: "6",
 		label: "Bags",
 		icon: BiShoppingBag,
-		path: "/category/bags",
+		path: "/#",
 	},
 	{
 		key: "7",
 		label: "Electronics",
 		icon: GiPlug,
-		path: "/category/electronics",
+		path: "/#",
 	},
 ];
 
--- src/common/head/NavBar.jsx
+++ src/common/head/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
 	GiHamburgerMenu,
 	GiPlug,
@@ -49,37 +49,37 @@ const Navbar = () => {
 			key: "2",
 			label: "Women",
 			icon: <FcBusinesswoman />,
-			path: "/category/women",
+			path: "/#",
 		},
 		{
 			key: "3",
 			label: "Children",
 			icon: <FaBaby />,
-			path: "/category/children",
+			path: "/#",
 		},
 		{
 			key: "4",
 			label: "Health & Beauty",
 			icon: <GiHealthIncrease />,
-			path: "/category/health",
+			path: "/#",
 		},
 		{
 			key: "5",
 			label: "Home & Lights",
 			icon: <GiOilySpiral />,
-			path: "/category/home-essentials",
+			path: "/#",
 		},
 		{
 			key: "6",
 			label: "Bags",
 			icon: <BiShoppingBag />,
-			path: "/category/bags",
+			path: "/#",
 		},
 		{
 			key: "7",
 			label: "Electronics",
 			icon: <GiPlug />,
-			path: "/category/electronics",
+			path: "/#",
 		},
 	];
 
