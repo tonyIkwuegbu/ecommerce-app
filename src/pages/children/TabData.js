5a1f6b5ee5daff94aec4a5ecdb6365eda2c2346d
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-07-18 21:07:11 +0100

    feat: add children category

---
+++ src/pages/children/TabData.js
@@ -0,0 +1,44 @@
+import { GiClothes, GiChelseaBoot } from "react-icons/gi";
+import { FcSportsMode } from "react-icons/fc";
+import { MdWatch } from "react-icons/md";
+import { FaSuitcase } from "react-icons/fa";
+import { DiGhostSmall } from "react-icons/di";
+
+export const NavChildren = [
+	{
+		title: "All",
+		id: 1,
+		icon: DiGhostSmall,
+		tabNum: 1,
+	},
+	{
+		title: "Cothing",
+		id: 2,
+		icon: GiClothes,
+		tabNum: 2,
+	},
+	{
+		title: "Shoes",
+		id: 3,
+		icon: GiChelseaBoot,
+		tabNum: 3,
+	},
+	{
+		title: "Sportswear",
+		id: 4,
+		icon: FcSportsMode,
+		tabNum: 4,
+	},
+	{
+		title: "Accessories",
+		id: 5,
+		icon: MdWatch,
+		tabNum: 5,
+	},
+	{
+		title: "Bags",
+		id: 6,
+		icon: FaSuitcase,
+		tabNum: 6,
+	},
+];
