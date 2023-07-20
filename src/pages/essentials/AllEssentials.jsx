2f58b98694af240644d5411be27ed8b6e09e2015
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-07-20 09:41:41 +0100

    feat: add essentials category

---
+++ src/pages/essentials/AllEssentials.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react";
+import { AiOutlineHeart, AiOutlinePlus } from "react-icons/ai";
+import { Empty } from "antd";
+import { add } from "../../store/cartSlice";
+import { useDispatch } from "react-redux";
+import ProductSkeleton from "../../components/ProductSkeleton";
+import { useNavigate } from "react-router-dom";
+
+const AllMen = ({ data, loading }) => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const [count, setCount] = useState([]);
+
+	// Set initial count values when data changes
+	useEffect(() => {
+		if (data && data.length > 0) {
+			setCount(Array(data.length).fill(0));
+		}
+	}, [data]);
+
+	/// ********************************* increase likes
+	const increment = (index) => {
+		const updatedCounts = [...count];
+		updatedCounts[index] += 1;
+		setCount(updatedCounts);
+	};
+
+	// ************************************* Dispatch handler
+	const addToCart = (productItem) => {
+		dispatch(add(productItem));
+	};
+
+	/// ******************************************** LOADING STATE
+	if (loading) {
+		return <ProductSkeleton />;
+	}
+
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 bg-[#f6f9fc]">
+			{data.length > 0 ? (
+				data.map((productItems, index) => (
+					<div className="" key={productItems.idl_product_code}>
+						<div className="product mt-[40px]">
+							<div className="img h-72">
+								<img
+									loading="lazy"
+									src={
+										productItems.main_picture === "" ||
+										productItems.main_picture === null
+											? "/images/essential-placeholder.jpeg"
+											: productItems.main_picture
+									}
+									alt={productItems.name}
+									onClick={() =>
+										navigate(
+											`/product/${productItems.idl_product_code}/${productItems.supplier_id}`,
+										)
+									}
+									className="cursor-pointer transition-all hover:scale-110 duration-500 ease-in-out w-full h-[250px] object-cover"
+								/>
+
+								<div className="product-like">
+									<label>{count[index]}</label> <br />
+									<AiOutlineHeart
+										onClick={() => increment(index)}
+										className="arrow"
+									/>
+								</div>
+							</div>
+							<div className="product-details p-2">
+								<h5 className="text-sm">
+									{productItems.name || productItems.model}
+								</h5>
+
+								<div className="price">
+									<h4>
+										<span>{productItems.currency} </span>{" "}
+										{productItems.retail_price}
+									</h4>
+									<button
+										onClick={() => addToCart(productItems)}
+										title="Add to cart"
+									>
+										<AiOutlinePlus className="mx-auto" />
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				))
+			) : (
+				<div className="flex items-center justify-center mx-auto h-screen">
+					<Empty className="" description="No Product available." />{" "}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default AllMen;
---
+++ src/pages/essentials/index.jsx
@@ -0,0 +1,140 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import Slider from "react-slick";
+import Axios from "axios";
+import { api } from "../../Api";
+import AllEssentials from "./AllEssentials";
+import { Essentials } from "./EssentialsData";
+import Appliances from "./Appliances";
+import Kitchen from "./Kitchen";
+import Dining from "./Dining";
+import Furnishing from "./Furnishing";
+// import MenCloth from "./MenCloth";
+// import MenShoes from "./MenShoes";
+// import SportsWear from "./Sportswear";
+// import MenWatches from "./MenWatches";
+// import MenBags from "./MenBags";
+
+const Men = () => {
+	const [slidesToShow, setSlidesToShow] = useState(6);
+	const [tabIndex, setTabIndex] = useState(1);
+	const [selected, setSelected] = useState(1);
+	const [productData, setProductData] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	// ******************************************************* GET PRODUCCT
+
+	const getProduct = useCallback(async () => {
+		setLoading(true);
+		try {
+			const fetchData = await Axios.get(
+				`${api.baseURL}/api/v1/ecommerce/products/category/home_essentials`,
+				{
+					headers: {
+						"Content-Type": "application/json",
+						"x-access-token": api.token,
+					},
+				},
+			);
+
+			setProductData(fetchData.data.data);
+			setLoading(false);
+		} catch (error) {
+			console.log(error);
+			setLoading(false);
+		}
+	}, []);
+	useEffect(() => {
+		getProduct();
+	}, [getProduct]);
+
+	// ************************************************* MOBILE SLIDER
+	const settings = useMemo(
+		() => ({
+			dots: false,
+			infinite: true,
+			speed: 500,
+			slidesToShow: slidesToShow,
+			slidesToScroll: 1,
+			autoplay: true,
+		}),
+		[slidesToShow],
+	);
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth >= 768) {
+				setSlidesToShow(6); // iPad view
+			} else {
+				setSlidesToShow(3); // Mobile view
+			}
+		};
+
+		// Set initial slidesToShow based on the current screen size
+		handleResize();
+
+		// Update slidesToShow when the window is resized
+		window.addEventListener("resize", handleResize);
+
+		// Cleanup event listener on component unmount
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+	return (
+		<div className="mt-4">
+			<div className="lg:hidden overflow-hidden py-2 font-semibold tracking-wider">
+				<Slider {...settings}>
+					{Essentials.map((item) => (
+						<div key={item.id} onClick={() => setSelected(item.tabNum)}>
+							<p
+								onClick={() => setTabIndex(item.tabNum)}
+								className="cursor-pointer"
+								style={selected === item.tabNum ? { color: "#ff5c00" } : null}
+							>
+								{item.title}
+							</p>
+						</div>
+					))}
+				</Slider>
+			</div>
+			<div className="max-w-[98%] m-auto flex space-x-4 justify-between bg-[#f6f9fc] p-4">
+				<div className="category hidden lg:inline-block">
+					{Essentials.map((value) => {
+						return (
+							<div
+								className="box cursor-pointer"
+								key={value.id}
+								onClick={() => setSelected(value.tabNum)}
+							>
+								<p
+									className="flex items-center leading-10"
+									onClick={() => setTabIndex(value.tabNum)}
+									style={
+										selected === value.tabNum ? { color: "#ff5c00" } : null
+									}
+								>
+									<span>
+										<value.icon className="text-lg" />
+									</span>
+									{value.title}
+								</p>
+							</div>
+						);
+					})}
+				</div>
+				<div>
+					{tabIndex === 1 && (
+						<AllEssentials data={productData} loading={loading} />
+					)}
+					{tabIndex === 2 && <Appliances />}
+					{tabIndex === 3 && <Kitchen />}
+					{tabIndex === 4 && <Dining />}
+					{tabIndex === 5 && <Furnishing />}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Men;
