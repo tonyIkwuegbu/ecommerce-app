93769818384b8ed2e437abcce870b4ee662050bd
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-07-24 18:25:12 +0100

    feat: add my account menu

---
+++ src/pages/account/index.jsx
@@ -0,0 +1,32 @@
+import { Tabs } from "antd";
+import ProfilePage from "./Profile";
+import Password from "./Password";
+import Transactions from "./Transactions";
+
+const { TabPane } = Tabs;
+
+const MyAccountPage = () => {
+	return (
+		<div className="flex items-center justify-center min-h-screen bg-gray-100">
+			<div className="w-full lg:w-[70%] bg-white shadow-lg rounded-md px-6">
+				<h1 className="text-center text-2xl font-bold mb-6 py-3">My Account</h1>
+				<div className="">
+					{" "}
+					<Tabs defaultActiveKey="profile">
+						<TabPane tab="Profile" key="profile">
+							<ProfilePage />
+						</TabPane>
+						<TabPane tab="Update Password" key="password">
+							<Password />
+						</TabPane>
+						<TabPane tab="My Transactions" key="transactions">
+							<Transactions />
+						</TabPane>
+					</Tabs>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default MyAccountPage;
