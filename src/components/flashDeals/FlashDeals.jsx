614bcb94c01e16ce9aedd3c95c8ef2cf4d954fa4
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-08-01 13:53:16 +0100

    refac: api call

--- src/components/flashDeals/FlashDeals.jsx
+++ src/components/flashDeals/FlashDeals.jsx
@@ -1,7 +1,52 @@
 import { AiFillThunderbolt } from "react-icons/ai";
 import FlashCard from "./FlashCard";
+import { useCallback, useEffect, useState } from "react";
+import { api } from "../../Api";
+import Axios from "axios";
+import ShuffleArray from "../../utils/Shuffle";
 
-const FlashDeals = ({ productItems }) => {
+const FlashDeals = () => {
+	const [productData, setProductData] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	// ******************************************************* GET TOP DEALS PRODUCTS
+
+	const getProduct = useCallback(async () => {
+		setLoading(true);
+		try {
+			const fetchData = await Axios.get(
+				`${api.baseURL}/api/v1/ecommerce/products/popular`,
+				{
+					headers: {
+						"Content-Type": "application/json",
+						"x-access-token": api.token,
+					},
+				},
+			);
+			setProductData(fetchData.data.data);
+			setLoading(false);
+		} catch (error) {
+			console.log(error);
+			setLoading(false);
+		}
+	}, []);
+	useEffect(() => {
+		getProduct();
+	}, [getProduct]);
+
+	useEffect(() => {
+		// Function to shuffle the product data and update the state
+		const shuffleProducts = () => {
+			const shuffledProducts = ShuffleArray([...productData]);
+			setProductData(shuffledProducts);
+		};
+
+		// Shuffle products every 1 hour (3600000 milliseconds)
+		const interval = setInterval(shuffleProducts, 3600000);
+
+		// Cleanup interval on component unmount
+		return () => clearInterval(interval);
+	}, [productData]);
 	return (
 		<>
 			<section className="py-[50px] bg-[#f6f9fc] px-0">
@@ -12,7 +57,7 @@ const FlashDeals = ({ productItems }) => {
 							Best Sellers
 						</h1>
 					</div>
-					<FlashCard productItems={productItems} />
+					<FlashCard loading={loading} productData={productData} />
 				</div>
 			</section>
 		</>
