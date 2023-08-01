cb8980b438693edb7207b4222f724e50d874bc7f
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-08-01 13:53:01 +0100

    feat: add new arrivals category

---
+++ src/components/newarrivals/Arrivals.jsx
@@ -0,0 +1,83 @@
+import { useCallback, useEffect, useState } from "react";
+import ArrivalsCard from "./ArrivalsCard";
+import { IoMdArrowDropright } from "react-icons/io";
+import { api } from "../../Api";
+import Axios from "axios";
+import ShuffleArray from "../../utils/Shuffle";
+
+const Arrivals = () => {
+	const [productData, setProductData] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	// ******************************************************* GET TOP DEALS PRODUCTS
+
+	const getProduct = useCallback(async () => {
+		setLoading(true);
+		try {
+			const fetchData = await Axios.get(
+				`${api.baseURL}/api/v1/ecommerce/products/newarrival`,
+				{
+					headers: {
+						"Content-Type": "application/json",
+						"x-access-token": api.token,
+					},
+				},
+			);
+
+			setProductData(fetchData.data.data);
+			setLoading(false);
+		} catch (error) {
+			console.log(error);
+			setLoading(false);
+		}
+	}, []);
+
+	useEffect(() => {
+		// Fetch product data when the component mounts
+		getProduct();
+	}, [getProduct]);
+
+	useEffect(() => {
+		// Function to shuffle the product data and update the state
+		const shuffleProducts = () => {
+			const shuffledProducts = ShuffleArray([...productData]);
+			setProductData(shuffledProducts);
+		};
+
+		// Shuffle products every 1 hour (3600000 milliseconds)
+		const interval = setInterval(shuffleProducts, 3600000);
+
+		// Cleanup interval on component unmount
+		return () => clearInterval(interval);
+	}, [productData]);
+
+	return (
+		<>
+			<section className="Discount background">
+				<div className="max-w-[90%] m-auto">
+					<div className="heading flex items-center justify-between border-b-2 mb-3">
+						<div className="heading-left row flex items-center space-x-2  underline decoration-[2px] underline-offset-[14px] decoration-[#ff5c00]">
+							<img
+								src="https://img.icons8.com/glyph-neue/64/26e07f/new.png"
+								alt=""
+								className="w-[40px] h-[40px] mt-[5px]"
+							/>
+							<h1 className="text-xl md:text-2xl font-semibold tracking-wide">
+								New Arrivals
+							</h1>
+						</div>
+						<div className="cursor-pointer flex items-center">
+							<span className="text-[#ff5c00] font-semibold text-sm">
+								View all
+							</span>
+							<IoMdArrowDropright className="text-[20px]" />
+						</div>
+					</div>
+					<ArrivalsCard loading={loading} productData={productData} />
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default Arrivals;
---
+++ src/components/newarrivals/ArrivalsCard.jsx
@@ -0,0 +1,91 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useEffect, useMemo, useState } from "react";
+import SlideSkeleton from "../SlideSkeleton";
+
+const ArrivalsCard = ({ loading, productData }) => {
+	const [slidesToShow, setSlidesToShow] = useState(6);
+
+	const settings = useMemo(
+		() => ({
+			dots: false,
+			infinite: true,
+			speed: 500,
+			slidesToShow: slidesToShow,
+			slidesToScroll: 1,
+			autoplay: true,
+		}),
+		[slidesToShow],
+	);
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth >= 1024) {
+				setSlidesToShow(6); // Desktop view
+			} else if (window.innerWidth >= 768) {
+				setSlidesToShow(4); // iPad view
+			} else {
+				setSlidesToShow(2); // Mobile view
+			}
+		};
+
+		// Set initial slidesToShow based on the current screen size
+		handleResize();
+
+		// Update slidesToShow when the window is resized
+		window.addEventListener("resize", handleResize);
+
+		// Cleanup event listener on component unmount
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+	/// ************************************ CURRENCY FORMAT
+	const formattedAmount = new Intl.NumberFormat("en-NG", {
+		style: "currency",
+		currency: "NGN",
+	});
+
+	/// ******************************************** LOADING STATE
+	if (loading) {
+		return <SlideSkeleton />;
+	}
+
+	return (
+		<>
+			<Slider {...settings}>
+				{productData?.length > 0 &&
+					productData?.map((value) => (
+						<div key={value.idl_product_code}>
+							<div className="box product">
+								<div className="h-[150px] w-[150px]">
+									<img
+										src={
+											value?.main_picture === "" ||
+											value?.main_picture === undefined
+												? "/images/home-placeholder.jpeg"
+												: value?.main_picture
+										}
+										alt={value?.name || value?.model || value?.brand}
+										className="transition-all hover:scale-110 duration-500 ease-in-out object-cover w-full h-full rounded"
+									/>
+								</div>
+								<div className="text-center py-3 tracking-wider">
+									<h4 className="text-sm font-semibold text-gray-600">
+										{value?.name || value?.model || value?.brand}
+									</h4>
+									<p className="text-[#ff5c00] font-semibold">
+										{formattedAmount.format(value?.retail_price)}
+									</p>
+								</div>
+							</div>
+						</div>
+					))}
+			</Slider>
+		</>
+	);
+};
+
+export default ArrivalsCard;
