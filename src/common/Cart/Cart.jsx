1f1e2b85b2015bcee9a2ba0b81ac6883b288fb36
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-08-17 23:03:48 +0100

    refac: update component

--- src/common/Cart/Cart.jsx
+++ src/common/Cart/Cart.jsx
@@ -1,41 +1,111 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import "./style.css";
 import { useSelector, useDispatch } from "react-redux";
-import { remove, add, decreaseQty } from "../../store/cartSlice";
-import { Button, Divider, Empty, Modal, message } from "antd";
+import { remove } from "../../store/cartSlice";
+import { Button, Divider, Empty, Modal, Spin, message } from "antd";
 import { MdDeleteForever } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import Axios from "axios";
 import { api } from "../../Api";
 import { getSummary } from "../../store/orderSlice";
+import { useCart } from "../../utils/CartUtils";
 
 const Cart = () => {
 	const navigate = useNavigate();
 	const cartItems = useSelector((state) => state.cart);
+	const user = useSelector((state) => state.auth.user);
 	const dispatch = useDispatch();
 	const [loading, setLoading] = useState(false);
+	const [loadingCart, setLoadingCart] = useState(false);
 	const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 	const [deleteModalVisible, setDeleteModalVisible] = useState(false);
 	const [itemToDelete, setItemToDelete] = useState(null);
+	const [userCartItems, setUserCartItems] = useState([]);
+	const [itemQuantities, setItemQuantities] = useState({});
+	const { fetchUserCart } = useCart();
 
-	// ************ cal total of items
-	const totalPrice = cartItems.reduce(
-		(price, item) => price + item.qty * item.retail_price,
+	const allItems = (isAuthenticated ? userCartItems : cartItems) || [];
+
+	// ******************************************************* GET AUTH USER CART ITEMS
+	useEffect(() => {
+		if (isAuthenticated) {
+			setLoadingCart(true);
+
+			fetchUserCart(user)
+				.then((cartItems) => {
+					setUserCartItems(cartItems);
+					setLoadingCart(false);
+				})
+				.catch((error) => {
+					// Handle error
+					console.error("Error fetching user's cart:", error);
+					setLoadingCart(false);
+				});
+		}
+	}, [isAuthenticated, user, fetchUserCart]);
+
+	if (loadingCart) {
+		return (
+			<div className="flex items-center justify-center mx-auto h-screen">
+				<Spin />
+			</div>
+		);
+	}
+
+	// *************************************************** cal total of items
+	const totalPrice = allItems.reduce(
+		(total, item) =>
+			total + (itemQuantities[item.idl_product_code] || 1) * item.naira_price,
 		0,
 	);
 
-	// ************ cal item total price
+	// ************************************************** cal item total price
 	const renderPrice = (item) => {
-		const productQty = item.retail_price * item.qty;
+		const productQty =
+			(itemQuantities[item.idl_product_code] || 1) * item.naira_price;
 		return productQty;
 	};
 
-	// ************ remove handler
+	// **************************************************** removeItems handler
 	const handleConfirmDelete = () => {
 		if (itemToDelete) {
-			dispatch(remove(itemToDelete.idl_product_code));
-			setDeleteModalVisible(false);
+			if (isAuthenticated) {
+				// For authenticated users, make an API call to delete the item
+				const email = user.email;
+				const idl_product_code = itemToDelete.idl_product_code;
+
+				Axios.delete(`${api.baseURL}/api/v1/ecommerce/cart/record/${email}`, {
+					data: { idl_product_code },
+					headers: {
+						"x-access-token": api.token,
+					},
+				})
+					.then((response) => {
+						if (response.data && response.data.status === true) {
+							//Item deleted successfully from the API
+							fetchUserCart(user)
+								.then((cartItems) => {
+									setUserCartItems(cartItems); // Update the cart items
+									message.success(response.data.message);
+								})
+								.catch((error) => {
+									console.error("Error fetching user's cart:", error);
+								});
+						} else {
+							message.error(response.data.message || "Failed to delete item.");
+						}
+					})
+					.catch((error) => {
+						console.error("Error deleting item from API:", error);
+						message.error("An error occurred while deleting item.");
+					});
+			} else {
+				// For non-authenticated users, dispatch the remove action directly
+				dispatch(remove(itemToDelete.idl_product_code));
+			}
+
+			setDeleteModalVisible(false); // Close the modal after confirming deletion
 		}
 	};
 
@@ -49,24 +119,15 @@ const Cart = () => {
 		setDeleteModalVisible(true);
 	};
 
-	// ************************************* Dispatch handler
-	const addToCart = (productItem) => {
-		dispatch(add(productItem));
-	};
-
 	// ****************************************************HANDLE EMPTY CART
 
-	if (cartItems.length === 0) {
+	if (allItems.length === 0) {
 		return (
 			<div className="flex items-center justify-center mx-auto h-screen">
 				<Empty className="" description="No items in cart." />{" "}
 			</div>
 		);
 	}
-
-	const handleContinueShopping = () => {
-		navigate("/");
-	};
 
 	/// ************************************ CURRENCY FORMAT
 	const formattedAmount = new Intl.NumberFormat("en-NG", {
@@ -75,7 +136,6 @@ const Cart = () => {
 	});
 
 	//***********************************************GENERATE ORDER ID AND GO TO CHECKOUT
-
 	const generateID = async () => {
 		setLoading(true);
 		if (!isAuthenticated) {
@@ -139,134 +199,153 @@ const Cart = () => {
 			});
 	};
 
+	//**************************** */ Function to update quantity for a specific cart item
+	const updateItemQuantity = (itemId, newQuantity) => {
+		setItemQuantities((prevQuantities) => ({
+			...prevQuantities,
+			[itemId]: newQuantity,
+		}));
+	};
+
 	return (
 		<section className="max-w-[98%]">
 			<div className="flex items-center justify-between p-2">
 				<h3 className="text-lg lg:text-3xl font-semibold text-gray-500">
 					Shopping Cart
 				</h3>
-				<Button type="default" className="" onClick={handleContinueShopping}>
+				<Button type="default" className="" onClick={() => navigate("/")}>
 					Continue Shopping
 				</Button>
 			</div>
 			<Divider />
 			<div className="flex flex-col lg:flex-row justify-between  overflow-hidden">
 				<div className="mx-4 w-auto lg:w-[70%]">
-					{cartItems &&
-						cartItems.length > 0 &&
-						cartItems?.map((cartItem) => (
-							<div
-								key={cartItem.idl_product_code}
-								className="grid grid-cols-1 lg:grid-cols-2 items-start gap-x-10 shadow-lg p-6 rounded-md my-6 h-auto bg-white"
-							>
-								<div className="grid grid-cols-1 lg:grid-cols-2 ">
-									<img
-										src={
-											cartItem.main_picture === "" ||
-											cartItem.main_picture === null
-												? "/images/home-placeholder.jpeg"
-												: cartItem.main_picture
-										}
-										alt={cartItem.name || cartItem.model}
-										className="pb-8 lg:pb-0"
-										style={{ width: "100px", height: "auto" }}
-									/>
+					{allItems?.map((cartItem) => (
+						<div
+							key={cartItem.idl_product_code}
+							className="grid grid-cols-1 lg:grid-cols-2 items-start gap-x-10 shadow-lg p-6 rounded-md my-6 h-auto bg-white"
+						>
+							<div className="grid grid-cols-1 lg:grid-cols-2 ">
+								<img
+									src={
+										cartItem.main_picture === "" ||
+										cartItem.main_picture === null
+											? "/images/home-placeholder.jpeg"
+											: cartItem.main_picture
+									}
+									alt={cartItem.name}
+									className="pb-8 lg:pb-0"
+									style={{ width: "100px", height: "auto" }}
+								/>
 
-									<div className="flex-col">
-										<h4 className="text-gray-500 text-xs lg:text-sm">
-											{cartItem.name || cartItem.model || cartItem.brand}
-										</h4>
+								<div className="flex-col">
+									<h4 className="text-gray-500 text-xs lg:text-sm">
+										{cartItem.name}
+									</h4>
 
-										<p className="font-semibold text-xs lg:sm py-3">
-											Sold by{" "}
-											<abbr className="text-[#ff5c00]">
-												{cartItem.supplier_name}
-											</abbr>
-										</p>
-										<p className="font-semibold text-xs lg:sm py-1">
-											Size:{" "}
-											<abbr className="text-[#ff5c00]">
-												{cartItem.size || "N/A"}
-											</abbr>
-										</p>
-									</div>
-								</div>
-
-								<div className="grid grid-cols-1 items-center tracking-wider font-semibold">
-									<div className="flex items-center space-x-12">
-										<p className="text-xs">
-											Quantity:{" "}
-											<span className="text-green-600">
-												<abbr className="text-xs">X</abbr> {cartItem.qty}{" "}
-											</span>{" "}
-										</p>
-										<div className="flex space-x-3 justify-end">
-											<button
-												className="incCart p-1"
-												onClick={() => addToCart(cartItem)}
-											>
-												{" "}
-												<AiOutlinePlus
-													className="mx-auto"
-													title="increase qty"
-												/>{" "}
-											</button>
-											<button
-												className="desCart p-1"
-												onClick={() =>
-													dispatch(decreaseQty(cartItem.idl_product_code))
-												}
-											>
-												{" "}
-												<AiOutlineMinus
-													className="mx-auto"
-													title="decrease qty"
-												/>{" "}
-											</button>{" "}
-										</div>
-									</div>
-
-									<p className="text-xs pt-2">
-										Price:{" "}
-										<span className="text-[#ff5c00]">
-											{formattedAmount.format(renderPrice(cartItem))}
-										</span>{" "}
+									<p className="font-semibold text-xs lg:sm py-3">
+										Color{" "}
+										<abbr className="text-gray-600">{cartItem.colour}</abbr>
+									</p>
+									<p className="font-semibold text-xs lg:sm py-1">
+										Size:{" "}
+										<abbr className="text-[#ff5c00]">
+											{cartItem.size || "N/A"}
+										</abbr>
 									</p>
 								</div>
-								<MdDeleteForever
-									title="remove from cart"
-									onClick={() => removeFromCart(cartItem)}
-									className="text-red-500 mt-6 text-xl hover:animate-pulse cursor-pointer"
-								/>
-								<Modal
-									title="Confirm Delete"
-									visible={deleteModalVisible}
-									onCancel={handleCancelDelete}
-									footer={[
-										<Button key="cancel" onClick={handleCancelDelete}>
-											Cancel
-										</Button>,
-										<Button
-											key="delete"
-											type="danger"
-											onClick={handleConfirmDelete}
-											className="text-red-500 font-semibold"
-										>
-											Delete
-										</Button>,
-									]}
-								>
-									Are you sure you want to remove this item from the cart?
-								</Modal>
 							</div>
-						))}
+
+							<div className="grid grid-cols-1 items-center tracking-wider font-semibold">
+								<div className="flex items-center space-x-12">
+									<p className="text-xs">
+										Quantity:{" "}
+										<span className="text-green-600">
+											<abbr className="text-xs">X</abbr>{" "}
+											{itemQuantities[cartItem.idl_product_code] || 1}{" "}
+										</span>{" "}
+									</p>
+									<div className="flex space-x-3 justify-end">
+										<button
+											className="incCart p-1"
+											onClick={() => {
+												const newQuantity =
+													(itemQuantities[cartItem.idl_product_code] || 1) + 1;
+												updateItemQuantity(
+													cartItem.idl_product_code,
+													newQuantity,
+												);
+											}}
+										>
+											{" "}
+											<AiOutlinePlus
+												className="mx-auto"
+												title="increase qty"
+											/>{" "}
+										</button>
+										<button
+											className="desCart p-1"
+											onClick={() => {
+												const currentQuantity =
+													itemQuantities[cartItem.idl_product_code] || 1;
+												const newQuantity =
+													currentQuantity > 1 ? currentQuantity - 1 : 1;
+												updateItemQuantity(
+													cartItem.idl_product_code,
+													newQuantity,
+												);
+											}}
+										>
+											{" "}
+											<AiOutlineMinus
+												className="mx-auto"
+												title="decrease qty"
+											/>{" "}
+										</button>{" "}
+									</div>
+								</div>
+
+								<p className="text-xs pt-2">
+									Price:{" "}
+									<span className="text-[#ff5c00]">
+										{formattedAmount.format(renderPrice(cartItem))}
+									</span>{" "}
+								</p>
+							</div>
+							<MdDeleteForever
+								title="remove from cart"
+								onClick={() => removeFromCart(cartItem)}
+								className="text-red-500 mt-6 text-xl hover:animate-pulse cursor-pointer"
+							/>
+							<Modal
+								title="Confirm Delete"
+								open={deleteModalVisible}
+								onCancel={handleCancelDelete}
+								footer={[
+									<Button key="cancel" onClick={handleCancelDelete}>
+										Cancel
+									</Button>,
+									<Button
+										key="delete"
+										type="danger"
+										onClick={handleConfirmDelete}
+										className="text-red-500 font-semibold"
+									>
+										Delete
+									</Button>,
+								]}
+							>
+								Are you sure you want to remove this item from the cart?
+							</Modal>
+						</div>
+					))}
 				</div>
 
 				<div className="shadow-lg p-4 my-6 lg:inline-block justify-around tracking-wider w-auto lg:w-[25%] h-[55vh] bg-white">
 					<div className="flex items-center justify-between text-sm font-semibold">
 						<p className="text-[#ff5c40]">Order Summary</p>
 						<p>
-							{cartItems.length} {cartItems.length > 1 ? "Items" : "Item"}
+							{allItems.length} {allItems.length > 1 ? "Items" : "Item"}
 						</p>
 					</div>
 					<Divider />
