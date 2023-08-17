f40bcbb60bcb0b7c34fbc4a0134161f0d2c3f063
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-08-17 23:07:55 +0100

    refac: update category entries

--- src/pages/women/AllWomen.jsx
+++ src/pages/women/AllWomen.jsx
@@ -1,35 +1,55 @@
-import { useEffect, useState } from "react";
-import { AiOutlineHeart, AiOutlinePlus } from "react-icons/ai";
-import { Empty } from "antd";
-import { add } from "../../store/cartSlice";
-import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+import { Divider, Empty, Spin } from "antd";
+import { useDispatch, useSelector } from "react-redux";
 import ProductSkeleton from "../../components/ProductSkeleton";
 import { useNavigate } from "react-router-dom";
+import { IoIosBasket } from "react-icons/io";
+import { BsFillCartCheckFill } from "react-icons/bs";
+import { add } from "../../store/cartSlice";
+import { useCart } from "../../utils/CartUtils";
 
 const AllWomen = ({ data, loading }) => {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
-	const [count, setCount] = useState([]);
+	//const [count, setCount] = useState([]);
+	const user = useSelector((state) => state.auth.user);
+	const userIsAuthenticated = useSelector(
+		(state) => state.auth.isAuthenticated,
+	);
+	const [isLoading, setIsLoading] = useState(false);
+	const { addToCartApi } = useCart();
 
 	// Set initial count values when data changes
-	useEffect(() => {
-		if (data && data.length > 0) {
-			setCount(Array(data.length).fill(0));
-		}
-	}, [data]);
+	// useEffect(() => {
+	// 	if (data && data.length > 0) {
+	// 		setCount(Array(data.length).fill(0));
+	// 	}
+	// }, [data]);
 
 	/// ********************************* increase likes
-	const increment = (index) => {
-		const updatedCounts = [...count];
-		updatedCounts[index] += 1;
-		setCount(updatedCounts);
-	};
+	// const increment = (index) => {
+	// 	const updatedCounts = [...count];
+	// 	updatedCounts[index] += 1;
+	// 	setCount(updatedCounts);
+	// };
 
-	// ************************************* Dispatch handler
-	const addToCart = (productItem) => {
-		dispatch(add(productItem));
-	};
+	// ******************************************************HANDLE CART
+	const handleAddToCart = async (productItem) => {
+		if (!userIsAuthenticated) {
+			dispatch(add(productItem)); // Handle non-authenticated user's cart
+			return; // Exit the function early if the user is not authenticated
+		}
 
+		try {
+			setIsLoading(true);
+			await addToCartApi(user, productItem.idl_product_code);
+		} catch (error) {
+			console.log("Error adding item to cart:", error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 	/// ************************************ CURRENCY FORMAT
 	const formattedAmount = new Intl.NumberFormat("en-NG", {
 		style: "currency",
@@ -42,12 +62,12 @@ const AllWomen = ({ data, loading }) => {
 	}
 
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 bg-[#f6f9fc]">
+		<div className="grid grid-cols-1 md:grid-cols-3 bg-[#f6f9fc]">
 			{data.length > 0 ? (
-				data.map((productItems, index) => (
-					<div className="" key={index}>
-						<div className="product mt-[40px]">
-							<div className="img h-72">
+				data.map((productItems) => (
+					<div className="" key={productItems?.idl_product_code}>
+						<div className="group h-[96] w-[300px] p-[20px] m-[8px] shadow-md rounded-md bg-white relative">
+							<div className="h-[200px] w-[200px] mx-auto">
 								<img
 									loading="lazy"
 									src={
@@ -62,29 +82,46 @@ const AllWomen = ({ data, loading }) => {
 											`/product/${productItems.idl_product_code}/${productItems.supplier_id}`,
 										)
 									}
-									className="cursor-pointer transition-all hover:scale-110 duration-500 ease-in-out w-full h-[250px] object-cover"
+									className="transition-all hover:scale-110 duration-500 ease-in-out object-cover cursor-pointer w-full h-full rounded"
 								/>
+								<div className="absolute top-0 right-0 cursor-pointer  m-[10px] opacity-0 group-hover:opacity-100 transition-all duration-1000 ease-in-out">
+									<AiOutlineHeart className="mb-2" />
+									<IoIosBasket className="" />
+								</div>
 
-								<div className="product-like">
+								{/* <div className="product-like">
 									<label>{count[index]}</label> <br />
 									<AiOutlineHeart
 										onClick={() => increment(index)}
 										className="arrow"
 									/>
-								</div>
+								</div> */}
 							</div>
-							<div className="product-details p-2">
-								<h5 className="text-sm">
-									{productItems.name || productItems.brand}
-								</h5>
+							<Divider />
+							<div className="font-semibold tracking-wide ">
+								<h3 className="text-[13px] text-gray-600 py-1 text-center">
+									{productItems?.name}
+								</h3>
 
-								<div className="price">
-									<h4>{formattedAmount.format(productItems?.retail_price)}</h4>
+								<p className="py-2 text-[#232f3e] text-xs">
+									Size: <span>{productItems?.size || "N/A"}</span>
+								</p>
+
+								<div className="price flex items-center tracking-wider justify-between">
+									<h4 className="text-green-500">
+										{formattedAmount.format(productItems?.naira_price)}
+									</h4>
 									<button
-										onClick={() => addToCart(productItems)}
+										type="button"
+										onClick={() => handleAddToCart(productItems)}
 										title="Add to cart"
+										disabled={isLoading[productItems?.idl_product_code]}
 									>
-										<AiOutlinePlus className="mx-auto" />
+										{isLoading[productItems?.idl_product_code] ? (
+											<Spin size="small" />
+										) : (
+											<BsFillCartCheckFill className="mx-auto" />
+										)}
 									</button>
 								</div>
 							</div>
