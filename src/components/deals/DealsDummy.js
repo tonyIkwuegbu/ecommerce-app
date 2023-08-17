af34aa1eb5ec3ea717cdda677ebe9664981a6c05
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-08-17 23:00:46 +0100

    update data

---
+++ src/components/deals/DealsDummy.js
@@ -0,0 +1,22 @@
+export const dummy = [
+	{
+		cover: "./images/flash/flash-1.png",
+		title: "Nike",
+	},
+	{
+		cover: "./images/flash/flash-2.png",
+		title: "Hublot Watch",
+	},
+	{
+		cover: "./images/flash/flash-3.png",
+		title: "iTel 47T",
+	},
+	{
+		cover: "./images/flash/flash-4.png",
+		title: "SportsWatch",
+	},
+	{
+		cover: "./images/flash/flash-5.png",
+		title: "Shade",
+	},
+];
