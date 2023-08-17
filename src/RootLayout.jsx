ee129ad604ed866aabaa3be97ee19b89a1299ba1
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-08-17 23:02:05 +0100

    chores

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,6 +1,5 @@
 import Footer from "./common/footer/Footer";
 import Header from "./common/head/Header";
-import { Provider } from "react-redux";
 import store from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
@@ -10,15 +9,13 @@ let persistor = persistStore(store);
 const RootLayout = ({ children }) => {
 	return (
 		<>
-			<Provider store={store}>
-				<PersistGate loading={null} persistor={persistor}>
-					<Header />
-					<main>
-						<main>{children}</main>
-					</main>
-					<Footer />
-				</PersistGate>
-			</Provider>
+			<PersistGate loading={null} persistor={persistor}>
+				<Header />
+				<main>
+					<main>{children}</main>
+				</main>
+				<Footer />
+			</PersistGate>
 		</>
 	);
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { CartProvider } from "./utils/CartUtils.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<Provider store={store}>
+			<CartProvider>
+				<Router>
+					<App />
+				</Router>
+			</CartProvider>
+		</Provider>
 	</React.StrictMode>,
 );
