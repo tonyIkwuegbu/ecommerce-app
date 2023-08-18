b40d6e68f0cf9c62f2a1154b5ce920c67f6eb0cc
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-08-18 18:41:50 +0100

    refac: update component

--- src/common/checkout/Checkout.jsx
+++ src/common/checkout/Checkout.jsx
@@ -2,20 +2,34 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Button, Divider, Empty } from "antd";
 import CheckoutForm from "./CheckoutForm";
+// import { useEffect } from "react";
+// import { useCart } from "../../utils/CartUtils";
 
 const Checkout = () => {
 	const navigate = useNavigate();
-
+	//const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 	const orderDetails = useSelector((state) => state.order.order);
-	const cartItems = useSelector((state) => state.cart);
+	//const user = useSelector((state) => state.auth.user);
+	//const cartItems = useSelector((state) => state.cart);
+	//const [userCartItems, setUserCartItems] = useState([]);
+	//const { fetchUserCart } = useCart();
 
-	// ************ cal item total price
-	const renderPrice = (item) => {
-		const productQty = item.retail_price * item.qty;
-		return productQty;
-	};
+	//const allItems = (isAuthenticated ? orderDetails : cartItems) || [];
+	// ******************************************************* GET AUTH USER CART ITEMS
+	// useEffect(() => {
+	// 	if (isAuthenticated) {
+	// 		fetchUserCart(user)
+	// 			.then((cartItems) => {
+	// 				setUserCartItems(cartItems);
+	// 			})
+	// 			.catch((error) => {
+	// 				// Handle error
+	// 				console.error("Error fetching user's cart:", error);
+	// 			});
+	// 	}
+	// }, [isAuthenticated, user, fetchUserCart]);
 
-	if (cartItems.length === 0) {
+	if (orderDetails?.length === 0) {
 		return (
 			<div className="flex items-center justify-center mx-auto h-screen">
 				<Empty className="" description="No Order yet." />{" "}
@@ -56,58 +70,50 @@ const Checkout = () => {
 					</div>
 
 					<Divider />
-					<CheckoutForm cartItems={cartItems} />
+					<CheckoutForm />
 				</div>
 
 				<div className="shadow-lg p-4 my-6 lg:inline-block justify-around tracking-wider bg-white w-auto lg:w-[30%] h-[85vh]">
 					<div className="flex items-center justify-between text-sm font-semibold">
 						<p className="text-[#ff5c40]">Order Details</p>
 						<p>
-							{cartItems.length} {cartItems.length > 1 ? "Items" : "Item"}
+							{orderDetails?.products?.length}{" "}
+							{orderDetails?.products?.length > 1 ? "Items" : "Item"}
 						</p>
 					</div>
 					<Divider />
 					<div className="h-[35vh] overflow-y-scroll">
-						{cartItems &&
-							cartItems.length > 0 &&
-							cartItems?.map((cartItem) => (
+						{orderDetails &&
+							orderDetails?.products?.length > 0 &&
+							orderDetails?.products?.map((cartItem) => (
 								<div
 									key={cartItem.idl_product_code}
 									className="grid grid-cols-2 items-start gap-x-4  p-6 my-6 h-[30vh] bg-gray-50 rounded-sm"
 								>
 									<img
-										src={
-											cartItem.main_picture === "" ||
-											cartItem.main_picture === null
-												? "/images/placeholder.jpeg"
-												: cartItem.main_picture
-										}
-										alt={cartItem.name || cartItem.model || cartItem.brand}
+										src={cartItem.main_picture}
+										alt={cartItem.name}
 										className="pb-8 lg:pb-0 cov"
 										style={{ width: "100px", height: "auto" }}
 									/>
 
-									<div className="flex-col">
-										<h4 className="text-gray-500 text-xs">
-											{cartItem.name || cartItem.model || cartItem.brand}
-										</h4>
+									<div className="flex-col text-[11px] font-semibold">
+										<h4 className="text-gray-500">{cartItem.product_name}</h4>
 
-										<p className="font-semibold text-xs lg:sm py-3">
-											Sold by{" "}
-											<abbr className="text-[#ff5c00]">
-												{cartItem.supplier_name}
-											</abbr>
+										<p className=" py-2">
+											Size{" "}
+											<abbr className="text-[#ff5c00]">{cartItem.size}</abbr>
 										</p>
-										<p className="font-semibold text-xs lg:sm py-2">
-											Price:{" "}
-											<span className="text-green-600]">
-												{formattedAmount.format(renderPrice(cartItem))}
+										<p className=" py-1">
+											Unit Price:{" "}
+											<span className="text-green-600">
+												{formattedAmount.format(cartItem.amount)}
 											</span>{" "}
 										</p>
-										<p className="font-semibold text-xs lg:sm py-2">
+										<p className="py-1">
 											Quantity:{" "}
 											<span className="text-gray-600">
-												<abbr className="">X</abbr> {cartItem.qty}{" "}
+												<abbr className="">X</abbr> {cartItem.quantity}
 											</span>{" "}
 										</p>
 									</div>
@@ -118,14 +124,14 @@ const Checkout = () => {
 					<div className="flex items-center justify-between text-xs">
 						<p>Shipping Cost</p>
 						<h4 className="font-semibold">
-							&#8358; {orderDetails.shipping_cost}
+							{formattedAmount.format(orderDetails.shipping_cost)}
 						</h4>
 					</div>
 					<Divider />
 					<div className="flex items-center justify-between text-xs">
 						<p>Products Amount</p>
 						<h4 className="font-semibold">
-							&#8358; {orderDetails.products_amount}
+							{formattedAmount.format(orderDetails.products_amount)}
 						</h4>
 					</div>
 					<Divider />
@@ -133,7 +139,7 @@ const Checkout = () => {
 					<div className="flex items-center justify-between text-sm font-semibold">
 						<p className="">Total</p>
 						<h4 className="text-green-500">
-							&#8358; {orderDetails.total_amount}
+							{formattedAmount.format(orderDetails.total_amount)}
 						</h4>
 					</div>
 				</div>
