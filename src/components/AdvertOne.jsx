0622e8890a4a22d8f0871e2c8386516fac4cdfa7
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-09-01 11:32:20 +0100

    feat: add ads images

---
+++ src/components/AdvertOne.jsx
@@ -0,0 +1,20 @@
+const AdvertOne = () => {
+	return (
+		<div className="group flex flex-col lg:flex-row items-center justify-center gap-6 max-w-[98%]">
+			<img
+				loading="lazy"
+				src="/images/id3-banner2.jpg"
+				alt="banner-1"
+				className="hover:cursor-pointer hover:opacity-90"
+			/>
+			<img
+				loading="lazy"
+				src="/images/id3-banner3.jpg"
+				alt="banner-3"
+				className="hover:cursor-pointer hover:opacity-90"
+			/>
+		</div>
+	);
+};
+
+export default AdvertOne;
---
+++ src/components/AdvertTwo.jsx
@@ -0,0 +1,14 @@
+const AdvertTwo = () => {
+	return (
+		<div className="flex justify-center py-4">
+			<img
+				loading="lazy"
+				src="/images/id3-banner4.jpg"
+				alt="banner-3"
+				className="hover:cursor-pointer hover:opacity-95"
+			/>
+		</div>
+	);
+};
+
+export default AdvertTwo;
