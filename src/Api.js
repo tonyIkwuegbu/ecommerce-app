dca01b98efd498788cc81a73080db763d47c3bac
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-09-09 19:21:36 +0100

    refac: update utils

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,14 @@
+import axios from "axios";
+
+export const makeRequest = axios.create({
+	baseURL: import.meta.env.VITE_BASE_URL,
+	headers: {
+		"Content-Type": "application/json",
+		"x-access-token": import.meta.env.VITE_APP_TOKEN,
+	},
+});
+
+//temporal use for post request***
 export const api = {
 	baseURL: import.meta.env.VITE_BASE_URL,
 	token: import.meta.env.VITE_APP_TOKEN,
