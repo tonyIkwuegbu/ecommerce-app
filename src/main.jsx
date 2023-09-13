95c0f70a9b1240f801eb25789b6b358498778312
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-09-13 18:09:07 +0100

    chores

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,19 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { CartProvider } from "./utils/CartUtils.jsx";
+import { ModalProvider } from "./utils/ModalContext.jsx";
+import ProductModal from "./components/ProductModal.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<CartProvider>
-				<Router>
-					<App />
-				</Router>
+				<ModalProvider>
+					<Router>
+						<App />
+					</Router>
+					<ProductModal />
+				</ModalProvider>
 			</CartProvider>
 		</Provider>
 	</React.StrictMode>,
