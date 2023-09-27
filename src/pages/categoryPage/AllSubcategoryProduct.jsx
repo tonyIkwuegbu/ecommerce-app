1f0515dd343033c0262ea73213f1c69a362a5552
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2023-09-27 10:53:14 +0100

    fix

--- src/pages/categoryPage/AllSubcategoryProduct.jsx
+++ src/pages/categoryPage/AllSubcategoryProduct.jsx
@@ -42,6 +42,7 @@ const AllSubcategoryProduct = ({ selectedSubcategory, categoryName }) => {
 			.catch((err) => {
 				console.log(err);
 				setLoading(false);
+				setProductData([]);
 			});
 	}, [categoryName, selectedSubcategory]);
 
